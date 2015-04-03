6b776fbd7e4571faa33eb5d0dc1458ac8f0c7bb9
SousaItalo <italops94@gmail.com>
2015-04-03 19:33:27 -0300

    Substituição do pacote "Classes" pelo pacote "Modulo de Pessoas".

---
+++ NetBeansProjects/SistemaDeVendas/src/ModuloDePessoas/Funcionario.java
@@ -0,0 +1,26 @@
+package ModuloDePessoas;
+
+/**
+ *
+ * @author cesar.nascimento
+ */
+public class Funcionario extends Pessoa{
+    private int id;
+    private String admissao;
+    
+    public void setId(int id){
+        this.id = id;
+    }
+    
+    public int getId(){
+        return this.id;
+    }
+    
+    public void setAdmissao(String admissao){
+        this.admissao = admissao;
+    }
+    public String getAdmissao(){
+        return admissao;
+    }
+    
+}
