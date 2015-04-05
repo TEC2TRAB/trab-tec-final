01b97a9623186e9bc46217f6665b2316728644bb
SousaItalo <italops94@gmail.com>
2015-04-05 17:14:39 -0300

    Classe Cliente.java
    Criei a classe cliente que, assim como a classe funcionário, Herda da
    classe pessoa.

---
+++ NetBeansProjects/SistemaDeVendas/src/ModuloDePessoas/Cliente.java
@@ -0,0 +1,36 @@
+package ModuloDePessoas;
+/**
+ *
+ * @author Sousa, Italo.
+ */
+public class Cliente extends Pessoa{
+    private int id;
+    private String dataDeCadastro;
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the dataDeCadastro
+     */
+    public String getDataDeCadastro() {
+        return dataDeCadastro;
+    }
+
+    /**
+     * @param dataDeCadastro the dataDeCadastro to set
+     */
+    public void setDataDeCadastro(String dataDeCadastro) {
+        this.dataDeCadastro = dataDeCadastro;
+    }
+    
+}
