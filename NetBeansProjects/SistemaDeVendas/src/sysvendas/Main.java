bd9ac26010bec283426a2117424d58694e18709c
Cesar Rodrigues <rasec1287@hotmail.com>
2015-04-05 11:35:33 -0300

    Adicionado método para mudança do Look and Feel do projeto

--- NetBeansProjects/SistemaDeVendas/src/sysvendas/Main.java
+++ NetBeansProjects/SistemaDeVendas/src/sysvendas/Main.java
@@ -8,8 +8,12 @@ package sysvendas;
 
 import Classes.ConnectionFactory;
 import Interface.Menu;
+import java.awt.Color;
 import java.sql.Connection;
 import java.sql.SQLException;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /**
  *
@@ -21,13 +25,37 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Menu m = new Menu();
-        m.setVisible(true);
-        
         try (Connection con = new ConnectionFactory().getConnection()) {
             System.out.println("Conexão aberta!");
         } catch(SQLException e) {
             System.out.println(e);
         }
+        //exemplos mudança estilo Nimbus
+        UIManager.put( "nimbusBase", new Color( 81, 81, 81 ) );  
+        UIManager.put( "nimbusBlueGrey", new Color( 190, 190, 190 ) );  
+        UIManager.put( "control", new Color( 223, 223, 223 ) ); 
+        try {  
+            //Setar estilo que eu quero,ex: Trocar Nimbus por Windows,ou Metal no if
+            for ( LookAndFeelInfo info : UIManager.getInstalledLookAndFeels() ) {  
+                if ( "Nimbus".equals( info.getName() ) ) {  
+                    UIManager.setLookAndFeel( info.getClassName() );  
+                    break;  
+                }  
+            }
+        //ver os estilos que eu tenho instalado no projeto
+        for ( UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels() ) {  
+            System.out.println( info.getName() );  
+        } 
+        } catch(UnsupportedLookAndFeelException e){
+            e.printStackTrace();
+        } catch(ClassNotFoundException e){
+            e.printStackTrace();
+        } catch(InstantiationException e){
+            e.printStackTrace();
+        } catch(IllegalAccessException e){
+            e.printStackTrace();
+        }
+        Menu m = new Menu();
+        m.setVisible(true); 
     }
 }
