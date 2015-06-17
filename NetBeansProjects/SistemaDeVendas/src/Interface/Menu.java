37b3c2bf711bb2f1bee30a110991aad706352e2c
Cesar Rodrigues <rasec1287@hotmail.com>
2015-06-17 13:44:36 -0300

    Correção no path da imagens do Menu

--- NetBeansProjects/SistemaDeVendas/src/Interface/Menu.java
+++ NetBeansProjects/SistemaDeVendas/src/Interface/Menu.java
@@ -97,7 +97,7 @@ public class Menu extends javax.swing.JFrame {
         jPanel4.setPreferredSize(new java.awt.Dimension(320, 200));
         jPanel4.setVerifyInputWhenFocusTarget(false);
 
-        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Classes/vendas.png"))); // NOI18N
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/vendas.png"))); // NOI18N
 
         cadVenda.setFont(new java.awt.Font("Lato Medium", 0, 18)); // NOI18N
         cadVenda.setForeground(new java.awt.Color(255, 255, 255));
@@ -145,7 +145,7 @@ public class Menu extends javax.swing.JFrame {
         jPanel1.setBackground(new java.awt.Color(45, 198, 110));
         jPanel1.setPreferredSize(new java.awt.Dimension(320, 200));
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Classes/produtos.png"))); // NOI18N
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/produtos.png"))); // NOI18N
 
         cadProd.setFont(new java.awt.Font("Lato Medium", 0, 18)); // NOI18N
         cadProd.setForeground(new java.awt.Color(255, 255, 255));
@@ -179,19 +179,20 @@ public class Menu extends javax.swing.JFrame {
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabel1)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(54, 54, 54)
                 .addComponent(cadProd)
                 .addGap(44, 44, 44)
-                .addComponent(consProd))
+                .addComponent(consProd)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         jPanel5.setBackground(new java.awt.Color(46, 204, 113));
         jPanel5.setPreferredSize(new java.awt.Dimension(320, 200));
         jPanel5.setVerifyInputWhenFocusTarget(false);
 
-        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Classes/cliente.png"))); // NOI18N
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/cliente.png"))); // NOI18N
 
         cadCliente.setFont(new java.awt.Font("Lato Medium", 0, 18)); // NOI18N
         cadCliente.setForeground(new java.awt.Color(255, 255, 255));
@@ -225,20 +226,18 @@ public class Menu extends javax.swing.JFrame {
         );
         jPanel5Layout.setVerticalGroup(
             jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel5Layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(jLabel4))
             .addGroup(jPanel5Layout.createSequentialGroup()
                 .addGap(67, 67, 67)
                 .addComponent(cadCliente)
                 .addGap(41, 41, 41)
                 .addComponent(consCliente)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         jPanel3.setBackground(new java.awt.Color(45, 198, 110));
 
-        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Classes/funcionario.png"))); // NOI18N
+        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/funcionario.png"))); // NOI18N
         jLabel5.setText(" ");
 
         cadFunc.setFont(new java.awt.Font("Lato Medium", 0, 17)); // NOI18N
@@ -273,15 +272,13 @@ public class Menu extends javax.swing.JFrame {
         );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(jLabel5))
             .addGroup(jPanel3Layout.createSequentialGroup()
                 .addGap(65, 65, 65)
                 .addComponent(cadFunc)
                 .addGap(36, 36, 36)
                 .addComponent(consFunc)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addComponent(jLabel5, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         jPanel6.setBackground(new java.awt.Color(255, 255, 255));
